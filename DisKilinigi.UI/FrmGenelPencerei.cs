8b02b6255bda2502a80be3cbecb38eba556f5345
Batuhan Yazıcı <batuhan-yazici@outlook.com.tr>
2023-03-05 15:49:36 +0300

    asdsad

--- DisKilinigi.UI/FrmGenelPencerei.cs
+++ DisKilinigi.UI/FrmGenelPencerei.cs
@@ -48,11 +48,63 @@ namespace DisKilinigi.UI
         private void btnHastaBilgileriKaydet_Click(object sender, EventArgs e)
         {
 
+            foreach (Randevu item in randevuListesi)
+            {
+                if (item.Hasta.HastaAdSoyad==clboxHastaBilgileri.SelectedItem.ToString())
+                {
+                    item.Hasta.TelefonNumarasi = mtxtHastaBilgileriTelefonNo.Text;
+                    item.Islemler = cmboxHastaBilgileriGelisSebebi.SelectedItem;
+                    item.Hasta.EkstraAciklama = txtHastaBilgileriEkstraBilgiler.Text;
+                    item.RandevuDurumu = cmboxHastaBilgileriTedaviDurumu.SelectedItem;
+
+                }
+
+            }
+
         }
+
+        private void clboxHastaBilgileri_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (CheckBox item in clboxHastaBilgileri.CheckedItems)
+            {
+                txtHastaBilgileriHastaAdSoyad.Text = item.Tag.Randevu.Hasta.AdSoyad;
+                mtxtHastaBilgileriKimlikNumarası.Text = item.Tag.Randevu.Hasta.KimlikNumarasi;
+                mtxtHastaBilgileriDogumTarihi.Text = item.Tag.Randevu.Hasta.DogumTarihi;
+                mtxtHastaBilgileriTelefonNo.Text = item.Tag.Randevu.Hasta.TelefonNumarasi;
+                cmboxHastaBilgileriKanGrubu.DataSource = item.Tag.Randevu.Hasta.KanGrubu;
+                txtHastaBilgileriEkstraBilgiler.Text = item.Tag.Randevu.Hasta.EkstraAciklama;
+                cmboxHastaBilgileriGelisSebebi.DataSource = item.Tag.Randevu.Hasta.Islemler;
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private void btnGecmisTümTedaviler_Click(object sender, EventArgs e)
         {
 
         }
+
+     
     }
 }
