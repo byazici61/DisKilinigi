a54c97eb342a1dee4bf89be64b4c5f9ba24ca8b7
zahide <108192527+zahideuzun@users.noreply.github.com>
2023-03-05 15:52:30 +0300

    a3

--- DisKilinigi.UI/FrmGenelPencerei.cs
+++ DisKilinigi.UI/FrmGenelPencerei.cs
@@ -53,41 +53,50 @@ namespace DisKilinigi.UI
 		private void btnGecmisTümTedaviler_Click(object sender, EventArgs e)
 		{
 
-		}
-		private void Berkay()
-		{
-			doktorListesi.AddRange(new List<Doktor>()
-			{
-				new Doktor() {DoktorAdSoyad="Melike Memiş",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
-				new Doktor() {DoktorAdSoyad="Batuhan Yazıcı",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
-				new Doktor() {DoktorAdSoyad="Berkay Engin",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
-				new Doktor() {DoktorAdSoyad="Ela Güler",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
-				new Doktor() {DoktorAdSoyad="Zahide Uzun",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
-				new Doktor() {DoktorAdSoyad="Yekta Büyükkaya",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
-			});
-			if (Validasyon())
-			{
-				// todo textbox1 => txtEkstraAciklama , Kangrubu textbox atılacak
-				hastaListesi.Add(
-					new Hasta
-					{
-						HastaAdSoyad = txtHastanaIlkMuayeneAdSoyad.Text,
-						KimlikNumarasi = mtxtHastaBilgileriKimlikNumarası.Text,
-						DogumTarihi = mtxtHastanaIlkMuayeneDoğumTarihi.Text,
-						TelefonNumarasi = mtxtHastanaIlkMuayeneTelefonNumarasi.Text,
-						EkstraAciklama = textBox1.Text,
-						KanGrubu = ""
-					});
-			}
-			else
-			{
-				MessageBox.Show("Lütfen Tüm Bilgileri Eksiksiz Giriniz");
-			}
-		}
-		private bool Validasyon()
-		{
-			throw new NotImplementedException();
-		}
+        }
+        private void Berkay()
+        {
+	        doktorListesi.AddRange(new List<Doktor>()
+	        {
+		        new Doktor() {DoktorAdSoyad="Melike Memiş",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
+		        new Doktor() {DoktorAdSoyad="Batuhan Yazıcı",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
+		        new Doktor() {DoktorAdSoyad="Berkay Engin",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
+		        new Doktor() {DoktorAdSoyad="Ela Güler",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
+		        new Doktor() {DoktorAdSoyad="Zahide Uzun",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
+		        new Doktor() {DoktorAdSoyad="Yekta Büyükkaya",MezuniyetUniversitesi="...",UzmanlikAlani="..." },
+	        });
+	        if (Validasyon())
+	        {
+		        // todo textbox1 => txtEkstraAciklama , Kangrubu textbox atılacak
+		        hastaListesi.Add(
+			        new Hasta
+			        {
+				        HastaAdSoyad = txtHastanaIlkMuayeneAdSoyad.Text,
+				        KimlikNumarasi = mtxtHastaBilgileriKimlikNumarası.Text,
+				        DogumTarihi = mtxtHastanaIlkMuayeneDoğumTarihi.Text,
+				        TelefonNumarasi = mtxtHastanaIlkMuayeneTelefonNumarasi.Text,
+				        EkstraAciklama = textBox1.Text,
+				        KanGrubu = ""
+			        });
+	        }
+	        else
+	        {
+		        MessageBox.Show("Lütfen Tüm Bilgileri Eksiksiz Giriniz");
+	        }
+        }
+        private bool Validasyon()
+        {
+	        throw new NotImplementedException();
+        }
+		void HastayaYapilacakIslemleriOlustur()
+        {
+	        cmboxYapılacakIslem.Controls.Add(new ComboBox() { Text = "Dolgu", Tag = new Islem() { IslemAdi = "Dolgu", IslemUcreti = 100 } });
+	        cmboxYapılacakIslem.Controls.Add(new ComboBox() { Text = "Kanal Tedavisi", Tag = new Islem() { IslemAdi = "Kanal Tedavisi", IslemUcreti = 500 } });
+	        cmboxYapılacakIslem.Controls.Add(new ComboBox() { Text = "Diş Beyazlatma", Tag = new Islem() { IslemAdi = "Diş Beyazlatma", IslemUcreti = 100 } });
+	        cmboxYapılacakIslem.Controls.Add(new ComboBox() { Text = "Diş Taşı Temizliği", Tag = new Islem() { IslemAdi = "Diş Taşı Temizliği", IslemUcreti = 100 } });
+	        cmboxYapılacakIslem.Controls.Add(new ComboBox() { Text = "İmplant", Tag = new Islem() { IslemAdi = "İmplant", IslemUcreti = 100 } });
+	        cmboxYapılacakIslem.Controls.Add(new ComboBox() { Text = "Diş Teli", Tag = new Islem() { IslemAdi = "Diş Teli", IslemUcreti = 100 } });
+        }
 	}
 
 }
