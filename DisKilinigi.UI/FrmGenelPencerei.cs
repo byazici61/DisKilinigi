d955ce70ae7ab081f0bde5a5b11bb5ad33126a9f
Batuhan Yazıcı <batuhan-yazici@outlook.com.tr>
2023-03-05 19:33:39 +0300

    sdsfd

--- DisKilinigi.UI/FrmGenelPencerei.cs
+++ DisKilinigi.UI/FrmGenelPencerei.cs
@@ -212,7 +212,6 @@ namespace DisKilinigi.UI
                         lvHastaBilgileri.Items.Add(li);
 
                         //item.RandevuUcreti += item.Islemler.IslemUcreti;
-                        
                     }
                 }
 
